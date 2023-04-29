cfbec9d5a3bffdd5295dad1d1fd16b2a069bd6b4
ZaniAndrei <zaniandrei1@gmail.com>
2023-04-29 17:48:24 -0300

    Tudo migrado para o arquivo Fase1.js

--- src/Fase1.js
+++ src/Fase1.js
@@ -8,33 +8,43 @@ class Fase1 extends Phaser.Scene{
     {
         console.log('load spritesheet');
         this.load.spritesheet('player_sp', 'assets/spritesheets/player.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('knaiffs_sp', 'assets/spritesheets/knaiffs.png', {frameWidth: 64, frameHeight: 64 });
+
         console.log('load tile sheet');
         this.load.image('tiles', 'assets/maps/tilesheet.png');
+
         console.log('load map');
-        this.load.tilemapTiledJSON('themap', 'assets/maps/Mapa.json');
+        this.load.tilemapTiledJSON('themap', 'assets/maps/mapa2.json');
     }
 
 // função para criação dos elementos
-     create ()
+    create ()
     {
 
         // criação do mapa e ligação com a imagem (tilesheet)
         this.map = this.make.tilemap({ key: 'themap', tileWidth: 16, tileHeight: 16 });
-        this.tileset = this.map.addTilesetImage('tileset', 'tiles');
+        this.tileset = this.map.addTilesetImage('Minifantasy_ForgottenPlainsTiles', 'tiles');
 
         // criação das camadas
         this.groundLayer = this.map.createDynamicLayer('Chao', this.tileset, 0, 0);
         this.wallsLayer = this.map.createDynamicLayer('Parede', this.tileset, 0, 0);
         
-
+        
         // criação do rei
-        this.player = this.physics.add.sprite(100, 300, 'player_sp', 0);
+        this.player = this.physics.add.sprite(65, 750, 'player_sp', 0);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setZoom(2);
+
         this.player.body.width = 20;
 
-        this.wallsLayer.setCollisionBetween(60, 80, true);
-        this.physics.add.collider(this.player, this.wallsLayer, function(){
-            console.log("Colissssion!");
-        });
+        this.naiffsNPC = this.physics.add.sprite(65, 200, 'knaiffs_sp', 0);
+        //reduzindo a escala do npc(era muito grande)
+        this.naiffsNPC.setScale(0.5);
+        this.naiffsNPC.body.width = 20;
+        this.naiffsNPC.setFrame(27);
+
+        this.wallsLayer.setCollisionBetween(65, 750, true);
+        this.physics.add.collider(this.player, this.wallsLayer);
         
         
         this.keyA = this.input.keyboard.addKey('A');
@@ -53,14 +63,17 @@ class Fase1 extends Phaser.Scene{
 
 
 // update é chamada a cada novo quadro
-     update ()
+    update ()
         {
-        // velocidade horizontal
-        if (this.keyD?.isDown) {
-            this.player.setVelocityX(210);
-            this.player.anims.play('run', true);
+            let direction 
+         
+            if (this.keyD?.isDown) {
+                direction = 'right'
+                this.player.setVelocityX(210);
+                this.player.anims.play('run', true);
         }
         else if (this.keyA?.isDown) {
+            direction = 'left'
             this.player.setVelocityX(-210);
             this.player.anims.play('run', true);
         }
@@ -81,5 +94,8 @@ class Fase1 extends Phaser.Scene{
             this.player.setVelocityY(0); 
         }
         }
+
+
+        
     }
 
