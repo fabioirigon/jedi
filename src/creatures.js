e4a82df0eaade8a4c644197dc424c72cf383ae7c
Lucas Prado <lucascastilho2003@outlook.com>
2023-06-06 17:25:13 -0300

    Add files via upload

--- src/creatures.js
+++ src/creatures.js
@@ -1,25 +1,28 @@
 class Elf extends Phaser.GameObjects.Sprite {
+  constructor(scene, color, x, y) {
+    super(scene, x, y);
 
-    constructor (scene, color, x, y)
-    {
-        super(scene, x, y);
+    this.color = color;
 
-        this.color = color;
+    this.setTexture("elves");
+    this.setPosition(x, y);
 
-        this.setTexture('elves');
-        this.setPosition(x, y);
+    this.play(this.color + "Idle");
 
-        this.play(this.color + 'Idle');
+    scene.add.existing(this);
 
-        scene.add.existing(this);
+    this.on("animationcomplete", this.animComplete, this);
 
-        this.on('animationcomplete', this.animComplete, this);
+    this.alive = true;
 
-        this.alive = true;
+    var hx = this.color === "blue" ? 110 : -40;
 
-        var hx = (this.color === 'blue') ? 110 : -40;
+    this.hp = new HealthBar(scene, x - hx, y - 110);
 
-        this.hp = new HealthBar(scene, x - hx, y - 110);
-
-        this.timer = scene.time.addEvent({ delay: Phaser.Math.Between(1000, 3000), callback: this.fire, callbackScope: this });
-    }
+    this.timer = scene.time.addEvent({
+      delay: Phaser.Math.Between(1000, 3000),
+      callback: this.fire,
+      callbackScope: this,
+    });
+  }
+}
